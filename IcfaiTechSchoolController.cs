28fc8631163e7a8bc0f2b2e5cb6296de6766a483
odelu-web <159778096+odelu-web@users.noreply.github.com>
2024-02-19 10:45:44 +0530

    ICFAI Tech School Controller file commint by odel

---
+++ IcfaiTechSchoolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IFHEHyd_MVC.Controllers
+{
+    public class IcfaiTechSchoolController : Controller
+    {
+        // GET: IcfaiTechSchool
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
